43d00f87062cd1c0cba6a85bfac74dbddfc47060
Johannes Jacobs <johannes-jacobs@live.de>
2018-04-16 20:29:26 +0200

    Fixed my stupidness

--- src/vHackOS-API/Web/vhNetwork.cs
+++ src/vHackOS-API/Web/vhNetwork.cs
@@ -7,53 +7,5 @@ namespace vHackOS.Web
         internal vhNetwork() : base("network.php")
         {
         }
-
-        public NetworkResponse AddAntivirusNode()
-        {
-            var req = CreateRequest();
-
-            req.AddParam("action", "100");
-
-            var res = Execute(req);
-
-            return res;
-        }
-
-        public NetworkResponse AddFirewallNode()
-        {
-            var req = CreateRequest();
-
-            req.AddParam("action", "200");
-
-            var res = Execute(req);
-
-            return res;
-        }
-
-        public NetworkResponse UpgradeSingleNode(NodeType nodeType, int nodeNumber)
-        {
-            var req = CreateRequest();
-
-            req.AddParam("action", "500");
-            req.AddParam("node_type", ((int)nodeType).ToString());
-            req.AddParam("node_number", nodeNumber.ToString());
-
-            var res = Execute(req);
-
-            return res;
-        }
-
-        public NetworkResponse UpgradeX5Node(NodeType nodeType, int nodeNumber)
-        {
-            var req = CreateRequest();
-
-            req.AddParam("action", "600");
-            req.AddParam("node_type", ((int)nodeType).ToString());
-            req.AddParam("node_number", nodeNumber.ToString());
-
-            var res = Execute(req);
-
-            return res;
-        }
     }
 }
--- src/vHackOS-API/Web/vhServer.cs
+++ src/vHackOS-API/Web/vhServer.cs
@@ -26,5 +26,53 @@ namespace vHackOS.Web
 
             return await ExecuteAsync(req);
         }
+
+        public ServerResponse AddAntivirusNode()
+        {
+            var req = CreateRequest();
+
+            req.AddParam("action", "100");
+
+            var res = Execute(req);
+
+            return res;
+        }
+
+        public ServerResponse AddFirewallNode()
+        {
+            var req = CreateRequest();
+
+            req.AddParam("action", "200");
+
+            var res = Execute(req);
+
+            return res;
+        }
+
+        public ServerResponse UpgradeSingleNode(NodeType nodeType, int nodeNumber)
+        {
+            var req = CreateRequest();
+
+            req.AddParam("action", "500");
+            req.AddParam("node_type", ((int)nodeType).ToString());
+            req.AddParam("node_number", nodeNumber.ToString());
+
+            var res = Execute(req);
+
+            return res;
+        }
+
+        public ServerResponse UpgradeX5Node(NodeType nodeType, int nodeNumber)
+        {
+            var req = CreateRequest();
+
+            req.AddParam("action", "600");
+            req.AddParam("node_type", ((int)nodeType).ToString());
+            req.AddParam("node_number", nodeNumber.ToString());
+
+            var res = Execute(req);
+
+            return res;
+        }
     }
 }
